0b09b4dc8bc9759d1bc1185eb4fc3a51feca3fdc
student@itpark.ru <Julia@localhost>
2019-09-21 10:56:35 +0300

    Вариант со 100% возможностью оплаты бонусами

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('worced');
+
+const purchaseAmount = 1000;
+const bonuses = 2000;
+
+let total;
+if (purchaseAmount <= bonuses) {
+     total = 0;
+} else {
+     total = purchaseAmount - bonuses;
+}
+
+console.log(total);
